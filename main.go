83318e041b65fc57eeafc838b8d2ea8d947d3360
Hrip Enzh <hripenzh@users.noreply.github.com>
2026-04-20 02:20:21 +0800

    Use exit code 2 for chezmoi errors to distinguish from generic failures

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 		BuiltBy: "source",
 	}, os.Args[1:]); err != nil {
 		fmt.Fprintf(os.Stderr, "chezmoi: error: %v\n", err)
-		os.Exit(1)
+		// Use exit code 2 to distinguish chezmoi errors from other non-zero exits
+		os.Exit(2)
 	}
 }
