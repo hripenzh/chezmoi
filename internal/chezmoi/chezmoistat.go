a7c91d0b93d20edd332dbac09d1eb04e584334b3
Hrip Enzh <hripenzh@users.noreply.github.com>
2026-04-21 09:26:31 +0800

    feat: add ChezmoiStat type for file metadata abstraction

---
+++ internal/chezmoi/chezmoistat.go
@@ -0,0 +1,94 @@
+package chezmoi
+
+import (
+	"io/fs"
+	"time"
+)
+
+// A SourceStateEntryType is the type of a source state entry.
+type SourceStateEntryType int
+
+const (
+	SourceStateEntryTypeAbsent SourceStateEntryType = iota
+	SourceStateEntryTypeDir
+	SourceStateEntryTypeFile
+	SourceStateEntryTypeSymlink
+)
+
+// A ChezmoiStat contains information about a file or directory, similar to
+// fs.FileInfo but with additional chezmoi-specific fields.
+type ChezmoiStat struct {
+	name    string
+	size    int64
+	mode    fs.FileMode
+	modTime time.Time
+	isDir   bool
+}
+
+// NewChezmoiStat returns a new ChezmoiStat from an fs.FileInfo.
+func NewChezmoiStat(info fs.FileInfo) ChezmoiStat {
+	if info == nil {
+		return ChezmoiStat{}
+	}
+	return ChezmoiStat{
+		name:    info.Name(),
+		size:    info.Size(),
+		mode:    info.Mode(),
+		modTime: info.ModTime(),
+		isDir:   info.IsDir(),
+	}
+}
+
+// Name returns the name of the file.
+func (s ChezmoiStat) Name() string {
+	return s.name
+}
+
+// Size returns the size of the file in bytes.
+func (s ChezmoiStat) Size() int64 {
+	return s.size
+}
+
+// Mode returns the file mode bits.
+func (s ChezmoiStat) Mode() fs.FileMode {
+	return s.mode
+}
+
+// ModTime returns the modification time of the file.
+func (s ChezmoiStat) ModTime() time.Time {
+	return s.modTime
+}
+
+// IsDir reports whether the file is a directory.
+func (s ChezmoiStat) IsDir() bool {
+	return s.isDir
+}
+
+// IsRegular reports whether the file is a regular file.
+func (s ChezmoiStat) IsRegular() bool {
+	return s.mode.IsRegular()
+}
+
+// IsSymlink reports whether the file is a symbolic link.
+func (s ChezmoiStat) IsSymlink() bool {
+	return s.mode&fs.ModeSymlink != 0
+}
+
+// Type returns the SourceStateEntryType for this stat.
+func (s ChezmoiStat) Type() SourceStateEntryType {
+	switch {
+	case s.isDir:
+		return SourceStateEntryTypeDir
+	case s.mode&fs.ModeSymlink != 0:
+		return SourceStateEntryTypeSymlink
+	case s.mode.IsRegular():
+		return SourceStateEntryTypeFile
+	default:
+		return SourceStateEntryTypeAbsent
+	}
+}
+
+// Perm returns the Unix permission bits of the file.
+func (s ChezmoiStat) Perm() fs.FileMode {
+	return s.mode.Perm()
+}
